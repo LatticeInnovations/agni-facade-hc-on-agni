7d046e34383b789738d3d3ded0b1c362855bee72
Mansi Kalra <mansi.kalra23@gmail.com>
2026-04-14 10:24:09 +0530

    added lastupdated in query

--- controllers/reportTokenController.js
+++ controllers/reportTokenController.js
@@ -1,4 +1,5 @@
 let model = require('../models/index');
+const { Op } = require("sequelize");
 
 const getReportToken = async (req, res) => {
   try {
@@ -6,11 +7,46 @@ const getReportToken = async (req, res) => {
 
     let _offset = parseInt(queryParams._offset) || 0;
     let _count = parseInt(queryParams._count) || 200;
+    let _lastUpdated = queryParams._lastUpdated;
+
+    let whereCondition = {};
+    
+    if (_lastUpdated) {
+      const operator = _lastUpdated.substring(0, 2); // ge, gt, le, lt
+      const dateValue = new Date(_lastUpdated.substring(2));
+
+      let sequelizeOp;
+
+      switch (operator) {
+        case "ge":
+          sequelizeOp = Op.gte;
+          break;
+        case "gt":
+          sequelizeOp = Op.gt;
+          break;
+        case "le":
+          sequelizeOp = Op.lte;
+          break;
+        case "lt":
+          sequelizeOp = Op.lt;
+          break;
+        default:
+          sequelizeOp = Op.gt;
+      }
+
+      whereCondition.updatedAt = {
+        [sequelizeOp]: dateValue,
+      };
+    }
+
+    let order = [["updatedAt", "ASC"]];
 
     const result = await model.ReportToken.findAndCountAll({
       attributes: ["appointmentId", "token"],
+      where: whereCondition,
       offset: _offset,
       limit: _count,
+      order,
     });
 
     const hasMore = _offset + _count < result.count;
