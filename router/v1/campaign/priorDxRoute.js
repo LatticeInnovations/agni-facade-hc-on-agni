140c61d090a9324dcea85b79357962351815891b
tulika03 <tulika@thelattice.in>
2026-04-23 17:57:41 +0530

    prior Dx and medication history APIs

---
+++ router/v1/campaign/priorDxRoute.js
@@ -0,0 +1,12 @@
+let express = require("express");
+let router = express.Router();
+let dxController = require("../../../controllers/priorDxController")
+
+router.post("/",  dxController.savePriorDxData);
+
+router.get("/", dxController.getPriorDxData); 
+
+
+
+
+module.exports = router
