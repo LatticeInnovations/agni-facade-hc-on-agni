cb0426c0599431f8314c007eef869d641210a30a
tulika03 <tulika@thelattice.in>
2026-04-24 15:05:43 +0530

    intervention APIs for campaign

--- controllers/interventionController.js
+++ controllers/interventionController.js
@@ -1,6 +1,6 @@
 const { getPractitionerName } = require("../services/commonFunctions");
 let ServiceRequest = require("../class/ServiceRequest")
-const {buildFHIRResource, fetchResource, handleError, getTransformedResult} = require("../services/helperFunctions");
+const {buildFHIRResource, fetchResource, handleError, getTransformedResult, getAPIPath} = require("../services/helperFunctions");
 let axios = require("axios");
 let config = require("../config/nodeConfig");
 const bundleStructure = require("../services/bundleOperation")
@@ -10,46 +10,97 @@ const {validateRequest} = require("../utils/validateRequest");
 const { publishReportJob } = require("../middleware/reportPublisher");
 const { saveToken } = require("../services/email/tokenStore");
 
-const fetchMainEncounter = async (interventionData, token) => {
+const fetchMainEncounter = async (examData, token, mainEncounterType) => {
     const mainEncounter =   await fetchResource("Encounter", {
-         appointment: interventionData.appointmentId,
+         appointment: examData.appointmentId,
          _count: 5000,
          _include: "Encounter:appointment",
+         type: mainEncounterType
      }, token);
- 
  
      return mainEncounter
  }
 
+ function applyNonCampaignSideEffects(req) {
+        
+    req.queueMeta = {
+        data: req.data,
+        entity: "intervention",
+        requestType: "post",
+        apiName: "add-intervention",
+        tokenData: req.decoded
+      };
+}
+
+
 //  save Intervention data
 let saveInterventionData = async function (req, res) {    
     try {
+        const isCampaignPath = await getAPIPath(req);
+        console.log("check is it campaign path: ", isCampaignPath)
+
         const validatedBody = validateRequest(req.body, interventionSchema, res);
         if (!validatedBody) return;
-        req.queueMeta = {
-            data: req.data,
-            entity: "intervention",
-            requestType: "post",
-            apiName: "add-intervention",
-            tokenData: req.decoded
-          };
+
+        if (!isCampaignPath) applyNonCampaignSideEffects(req);
+        
         const token = req.accessToken;
         let resourceResult = [];
-        for (let interventionData of req.body) {
-                const encounterData = await fetchMainEncounter(interventionData, token)
+        const allResourceResults = [], errData = [];
+        const mainEncounterType = isCampaignPath ? "screening-site-main-encounter" : "facility-main-encounter";
+        const serviceRequestCode = isCampaignPath ? "screening-site-409073007": "409073007";
+        await Promise.all(
+
+            req.body.map(async (interventionData) =>  {
+                const encounterData = await fetchMainEncounter(interventionData, token, mainEncounterType);
+                console.log("encounterData: ", encounterData)
                 const reqUuid = interventionData.uuid;
                 const baseEncounterId = encounterData?.entry?.[0]?.resource?.id;
-                if (!baseEncounterId) return;    
-                const existingResponse = await fetchResource("ServiceRequest", {category: "409073007", encounter: baseEncounterId, _total: "accurate"}, token);
+                if (!baseEncounterId) {
+                    errData.push({
+                      status: 0,
+                      id: reqUuid,
+                      err: "Main encounter not found",
+                      fhirId: null,
+                    });
+                    return;
+                  }
+                const existingServiceResponse = await fetchResource("ServiceRequest", {category: serviceRequestCode, encounter: baseEncounterId, _total: "accurate"}, token);
+                const existingCount =  existingServiceResponse?.total ??  existingServiceResponse?.entry?.length ??  0;
+                if (existingCount > 0) {
+                console.info(
+                    `ServiceRequest already exists for encounter ${baseEncounterId}, skipping...`
+                );
+                errData.push({
+                    status: 0,
+                    id: reqUuid,
+                    err: "Intervention already exists for this encounter",
+                    fhirId:
+                    existingServiceResponse?.entry?.[0]?.resource?.id ?? null,
+                });
+                return;
+                }
+
                 interventionData.activityList = interventionData.interventions.map(e => "ActivityDefinition/" + e)
-                const interventionResponseResource = buildFHIRResource(ServiceRequest, {...interventionData, categoryCode: "409073007", categoryDisplay: "Interventions" ,encounterId: baseEncounterId, practitionerId: req.decoded.userId})
+                const interventionResponseResource = buildFHIRResource(ServiceRequest, {...interventionData, categoryCode: serviceRequestCode, categoryDisplay: "Interventions" ,encounterId: baseEncounterId, practitionerId: req.decoded.userId})
                 interventionResponseResource.uuid = reqUuid
                 const interventionResponseBundle =await  bundleStructure.setBundlePost(interventionResponseResource, interventionResponseResource.identifier, interventionData.uuid, "POST", "identifier")
-                resourceResult.push(interventionResponseBundle)
+                resourceResult.push(interventionResponseBundle);
+                allResourceResults.push(...resourceResult);
         }             
            
+            ));
+
+            if (allResourceResults.length === 0) {
+                return res.status(200).json({
+                    status: 1,
+                    message: "Intervention data saved.",
+                    data: errData
+                });
+            }
+
             let bundleData = await bundleStructure.getBundleJSON({resourceResult})  
-                    // return res.status(201).json({ status: 1, message: "Intervention data saved.", data: bundleData.bundle })
+            // return res.status(201).json({ status: 1, message: "Intervention data saved.", data: bundleData.bundle })
             let response = await axios.post(config.baseUrl, bundleData.bundle, {
                 headers: {
                             'Authorization': `Bearer ${token}`,
@@ -63,7 +114,8 @@ let saveInterventionData = async function (req, res) {
                 for (const patientId of patientIds) {
                     await publishReportJob(patientId);
                 }  
-                let responseData = setInterventionSaveResponse(bundleData.bundle.entry, response.data.entry, "post");   
+                let resourceResponse = setInterventionSaveResponse(bundleData.bundle.entry, response.data.entry, "post");  
+                   const responseData = [...resourceResponse, ...errData];   
                 res.status(201).json({ status: 1, message: "Intervention data saved.", data: responseData })
             }
             else {
@@ -77,31 +129,42 @@ let saveInterventionData = async function (req, res) {
 
 }
 
+function applyNonCampaignSideEffectsOnUpdate(req) {
+        
+    req.queueMeta = {
+        data: req.data,
+        entity: "intervention",
+        requestType: "put",
+        apiName: "update-intervention",
+        tokenData: req.decoded
+      };
+}
+
 
 //  save Intervention data
 let updateInterventionData = async function (req, res) {    
     try {
+        const isCampaignPath = await getAPIPath(req);
+        console.log("check is it campaign path: ", isCampaignPath)
+
         const validatedBody = validateRequest(req.body, interventionUpdateSchema, res);
         if (!validatedBody) return;
-        req.queueMeta = {
-            data: req.data,
-            entity: "intervention",
-            requestType: "put",
-            apiName: "update-intervention",
-            tokenData: req.decoded
-          };
+
+        if (!isCampaignPath) applyNonCampaignSideEffectsOnUpdate(req);
+
         const token = req.accessToken;
+        const category = isCampaignPath ? "screening-site-409073007" : "409073007"
         let resourceResult = [];
         for (let interventionData of req.body) {
                 const encounterData = await fetchMainEncounter(interventionData, token)
                 const reqUuid = interventionData.uuid;
                 const baseEncounterId = encounterData?.entry?.[0]?.resource?.id;
                 if (!baseEncounterId) return;    
-                const existingResponse = await fetchResource("ServiceRequest", {category: "409073007", encounter: baseEncounterId, _total: "accurate"}, token);
+                const existingResponse = await fetchResource("ServiceRequest", {category: category, encounter: baseEncounterId, _total: "accurate"}, token);
                 console.log("existingResponse: ", existingResponse)
                 interventionData.activityList = interventionData.interventions.map(e => "ActivityDefinition/" + e)
                     interventionData.uuid = existingResponse.entry[0].resource.identifier[0].value;
-                    const interventionResponseResource = buildFHIRResource(ServiceRequest, {...interventionData, categoryCode: "409073007", categoryDisplay: "interventions" ,encounterId: baseEncounterId, practitionerId: req.decoded.userId})
+                    const interventionResponseResource = buildFHIRResource(ServiceRequest, {...interventionData, categoryCode: category, categoryDisplay: "interventions" ,encounterId: baseEncounterId, practitionerId: req.decoded.userId})
                     interventionResponseResource.uuid = reqUuid;
                     const interventionResponseBundle = await bundleStructure.setBundlePut(interventionResponseResource, null, existingResponse.entry[0].resource.id, "PUT", "identifier")
                     resourceResult.push(interventionResponseBundle) 
@@ -149,10 +212,12 @@ const setInterventionSaveResponse  = (reqBundleData, responseBundleData, type) =
 
 let getInterventionData = async function (req, res) {
     try {
+        const isCampaignPath = await getAPIPath(req);
+        console.log("check is it campaign path: ", isCampaignPath)
         const token = req.accessToken;
         const link = config.baseUrl + "ServiceRequest";
         const queryParams = req.query
-        queryParams.category = "409073007"
+        queryParams.category =  isCampaignPath ? "screening-site-409073007" :"409073007"
         queryParams._total = "accurate";
         let resourceResult = []
         let resourceUrlData = { link: link, reqQuery: queryParams, allowNesting: 0, specialOffset: 1 }
@@ -171,11 +236,13 @@ let getInterventionData = async function (req, res) {
         interventionResponses.entry.forEach(interventionResponse => {            
             const responseObj = getTransformedResult(ServiceRequest, interventionResponse.resource);
             const primaryEncounter = mainEncounters.find((e) => e.id === interventionResponse.resource.encounter.reference.split("/")[1]);
-            responseObj.practitionerName = getPractitionerName(responseObj.practitionerId, practitionerList.entry);
+            responseObj.practitionerName = isCampaignPath ? null : getPractitionerName(responseObj.practitionerId, practitionerList.entry);
+            responseObj.practitionerId = isCampaignPath ? null : responseObj.practitionerId;
             responseObj.interventions = responseObj?.activityList || []
             delete responseObj["activityList"]
             responseObj.appointmentId = primaryEncounter?.appointment?.[0]?.reference?.split("/")[1] || null;
-            responseObj.appointmentUuid = primaryEncounter?.identifier?.[0].value
+            responseObj.appointmentUuid = primaryEncounter?.identifier?.[0].value;
+            responseObj.campaignId = isCampaignPath ? primaryEncounter.location[0].location.reference.split("/")[1]: null;
             resourceResult.push(responseObj)
         });
         
