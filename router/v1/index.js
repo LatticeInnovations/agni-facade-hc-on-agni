0b2ca979967dad8891ff15cb7afb0ddd96df7f10
Nishita koshta <110012128+nishitakoshta@users.noreply.github.com>
2026-04-27 15:46:39 +0530

    Remove unused report and reportToken routes
    Removed unused routes for report and reportToken.

--- router/v1/index.js
+++ router/v1/index.js
@@ -37,6 +37,4 @@ router.use("/Immunization", auth, require("./immunizationRoute"));
 router.use("/ImmunizationRecommendation", auth, require("./immunizationRecommendationRoute"));
 
 router.use("/campaign", auth, require("./campaign/index"));
-router.use("/", require("./reportRoute"));
-router.use("/reportToken", auth, require("./reportTokenRoute"));
 module.exports = router;
