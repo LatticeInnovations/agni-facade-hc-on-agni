703e1922b5cbd31365a7ab644c30ce7533a82053
tulika03 <tulika312@gmail.com>
2026-05-04 16:47:03 +0530

    script update

--- facade-heartcare-agni-scripts/scripts/campaign scripts/dev/appontment/appointment.js
+++ facade-heartcare-agni-scripts/scripts/campaign scripts/dev/appontment/appointment.js
@@ -12,6 +12,7 @@
  * Requirements:
  *   - Node.js 18+
  *   - A Appointment.json file in the same directory (FHIR Bundle or array of Appointment resources)
+ *   - Appointment?service-type:missing=true&_count=1000
  */
 
 "use strict";
